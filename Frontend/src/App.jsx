eada903f77a7dc49c716e62a9dfe5151d390cc46
Jatin <jatinrajvani.cg@gmail.com>
2025-06-02 15:24:21 +0530

    Adding a Protected Routes for employee and Hr pages. By giving a route To diffrent pageslike Dashboard,attendance and salary.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { BrowserRouter  , Route , Routes } from 'react-router-dom'
 import { useSelector } from "react-redux";
+import { Navigate } from 'react-router-dom';
 
 
 import Login from './Pages/Login/Login'
 import Home from './Pages/Home/Home'
 import HRDashboard from './Pages/Admin/HrDashboard'
-import Deshbord from './Pages/Employee/Home.jsx'
+import Dashbord from './Pages/Employee/EmployeeHome.jsx'
 import Attendance from './Pages/Employee/Attendance.jsx'
 import Add from './Pages/Add.jsx'
 import Salary from './Pages/Employee/Salary.jsx';
 import Calendar from './Pages/Employee/Calendar.jsx';
-
+import Sidebar from './Components/Sidebar'
 function App() {
 
     const isAuthenticated = localStorage.getItem("token");
@@ -26,14 +27,32 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-    {/* <Route path='/' element={<Login />} />
-    {isAuthenticated && role==='employee' && <Route path='/home' element={<Home />} /> }
+    <Route path='/' element={<Login />} />
+    {isAuthenticated && role==='employee' && 
+    (  <Route
+            path="/*"
+            element={
+              <div className="flex">
+                {/* <Sidebar /> */}
+                <div className="flex-1">
+                  <Routes>
+                    <Route path="/emhome" element={<Dashbord />} />
+                    <Route path="/emsalary" element={<Salary />} />
+                    <Route path="/emattendance" element={<Attendance />} />
+                    <Route path="/emcalendar" element={<Calendar />} />
+                     <Route path="/emprofile" element={<Calendar />} />
+                  </Routes>
+                </div>
+        
+              </div>
+            }
+          />)}
     {isAuthenticated && role==='hr' && <Route path='/hr' element={<HRDashboard />} />}
-    <Route path='/add' element={<Add />} /> */}
-    <Route path='/emhome' element={<Deshbord/>}/>
+    <Route path='/add' element={<Add />} />
+    {/* <Route path='/emhome' element={<Deshbord/>}/>
     <Route path='/emattendance' element={<Attendance/>}/>
     <Route path='/emsalary' element={<Salary/>}/>
-    <Route path='/emcalendar' element={<Calendar/>}/>
+    <Route path='/emcalendar' element={<Calendar/>}/> */}
     </Routes>
     </BrowserRouter>      
 
