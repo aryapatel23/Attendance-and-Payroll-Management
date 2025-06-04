1d58ae1be4ada6f72ce4bf2e91293e6ae9ed0296
arya patel <aryaunjha@gmail.com>
2025-06-04 09:42:46 +0530

    create the add employee option page for HR

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,8 +5,13 @@ import { useSelector } from 'react-redux';
 import Login from './Pages/Login/Login'
 import Home from './Pages/Home/Home'
 import HRDashboard from './Pages/Admin/HrDashboard'
-import Deshbord from './Pages/Employe/Home.jsx'
+import Deshbord from './Pages/Employee/EmployeeHome.jsx'
+import Attendance from './Pages/Employee/Attendance.jsx'
 import Add from './Pages/Add.jsx'
+import Employees from './Pages/Admin/Employees.jsx';
+import Salary from './Pages/Employee/Salary.jsx';
+import Calendar from './Pages/Employee/Calendar.jsx';
+import AddEmployee from './Pages/Admin/AddEmployee.jsx';
 
 function App() {
   const isAuthenticated = localStorage.getItem('token');
@@ -22,10 +27,15 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Login />} />
-    {isAuthenticated && role==='employee' && <Route path='/home' element={<Home />} /> }
-    {isAuthenticated && role==='hr' && <Route path='/hr' element={<HRDashboard />} />}
+    {isAuthenticated && role==='employee' && <Route path='/emhome' element={<Deshbord />} /> }
+    {isAuthenticated && role==='employee' && <Route path='/emattendance' element={<Attendance />} /> }
+    {isAuthenticated && role==='employee' && <Route path='/emsalary' element={<Salary />} /> }
+    {isAuthenticated && role==='employee' && <Route path='/emcalendar' element={<Calendar />} /> }
+    {isAuthenticated && role==='hr' && <Route path='/hrhome' element={<HRDashboard />} />}
+    {isAuthenticated && role==='hr' && <Route path='/hremployees' element={<Employees/>} />}
+    {isAuthenticated && role==='hr' && <Route path='/hraddemployee' element={<AddEmployee/>} />}
     <Route path='/add' element={<Add />} />
-    <Route path='/emhome' element={<Deshbord/>}/>
+
     </Routes>
     </BrowserRouter>      
 
