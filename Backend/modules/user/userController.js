c49f2d62b8761db60df84ddeb865b18a6ffd4d64
Jatin <jatinrajvani.cg@gmail.com>
2025-06-19 18:51:01 +0530

    Adding the request in backend for fetching the all users

--- Backend/modules/user/userController.js
+++ Backend/modules/user/userController.js
@@ -77,4 +77,20 @@ const addUser = async (req, res) => {
   }
 };
 
-module.exports = { getProfile, addUser };
+const alluser = async (req,res)=>{
+  const db = getDB();
+  try{
+    const users = await db.collection('users').find().project({ password: 0 }).toArray();
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: 'No users found' });
+    }
+    res.status(200).json({message:"User fetched sucessfully", users});
+    
+
+  }catch(error){
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
+module.exports = { getProfile, addUser, alluser };
--- Backend/modules/user/userRoutes.js
+++ Backend/modules/user/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getProfile,addUser } = require('./userController');
+const { getProfile,addUser,alluser } = require('./userController');
 const authenticateToken = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.get('/profile', authenticateToken, getProfile);
 router.post('/add', addUser);
+router.get('/all', alluser);
 
 module.exports = router;
