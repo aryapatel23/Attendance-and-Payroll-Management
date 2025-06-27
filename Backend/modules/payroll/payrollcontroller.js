0cac10ec9d13d3f0a0039d38e466f901535699aa
Jatin <jatinrajvani.cg@gmail.com>
2025-06-27 11:16:36 +0530

    Adding a tax components and Generating a slary like slip

--- Backend/modules/payroll/payrollcontroller.js
+++ Backend/modules/payroll/payrollcontroller.js
@@ -3,6 +3,7 @@ const { getDB } = require("../../config/db");
 const GenerateSlip = async (req, res) =>{
 
       const db = getDB();
+      console.log(db)
       const { user_id, month } = req.body;
       const user = await db.collection('users').findOne({ user_id });
       if (!user) return res.status(404).json({ message: 'User not found' });
@@ -21,19 +22,27 @@ const GenerateSlip = async (req, res) =>{
     const absentdays=workingDays-PresentDays
     console.log('Absent day is',absentdays)
     const paidleaves=2;
+    const tax=basicsalary*0.1
+    const pf=basicsalary*0.05
     let givensalary;
+    let totaldeduction;
+    let leavededuction;
     if(absentdays>paidleaves){
-    const leavededuction=(absentdays-paidleaves)*(basicsalary/workingDays);  
+    leavededuction=(absentdays-paidleaves)*(basicsalary/workingDays);  
+    totaldeduction=tax+pf+leavededuction
     console.log('Leavededuction is',leavededuction)
-    givensalary=basicsalary-leavededuction;
+    givensalary=basicsalary-totaldeduction;
     }else if(absentdays<paidleaves){
-    givensalary=basicsalary
-   
+    givensalary=basicsalary-totaldeduction;
     }
-    res.json({message:"Given salary is",givensalary}) 
-
-
-    
+    res.json({message:"Generated slip is",
+      "Total working days":workingDays,
+    "Total your work day is":workingDays-absentdays,
+  "tax is":tax,
+"pf is":pf,
+"leave deduction is":leavededuction,
+'total deduction is':totaldeduction,
+'given salary is':givensalary}) 
 }
 
 module.exports= {GenerateSlip};
