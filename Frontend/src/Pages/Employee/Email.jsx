04a730a72a2c38d6148aef0b3eefe8c57b3330cf
Jatin <jatinrajvani.cg@gmail.com>
2025-07-31 14:34:56 +0530

    Changing the link of the api to upload the image

--- Frontend/src/Pages/Employee/Email.jsx
+++ Frontend/src/Pages/Employee/Email.jsx
@@ -61,7 +61,7 @@ const SetPassword = () => {
     formData.append("image", image);
 
     try {
-      const response = await axios.post("http://localhost:5000/api/upload-profile", formData);
+      const response = await axios.post("http://localhost:5000/api/upload-image", formData);
       setImageUrl(response.data.imageUrl);
       setUploadMessage("✅ Profile image uploaded successfully.");
     } catch (error) {
